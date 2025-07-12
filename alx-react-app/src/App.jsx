84167aeec01231aed406a9bb3f201876582c08c6
Omy10 <naomimwangi382002@gmail.com>
2025-07-12 22:09:03 +0300

    second exercise

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,10 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './WelcomeMessage'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,7 +31,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="App">
+      <WelcomeMessage />
+    </div>
     </>
+    
   )
 }
 
