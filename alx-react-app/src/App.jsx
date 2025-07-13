56fd1735b8fb5f04f41d9481906fd4cc8d87a047
Omy10 <naomimwangi382002@gmail.com>
2025-07-13 21:55:25 +0300

    third exercise

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,10 +3,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage.jsx'
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
+import WelcomeMessage from './components/WelcomeMessage.jsx'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile.jsx'
 
 
 function App() {
@@ -37,6 +38,10 @@ function App() {
       <div className="App">
       <WelcomeMessage />
       <Header />, <MainContent />,<Footer />
+
+      <div className="App">
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
 
     </div>
 
